f44325249e419bcdfc53db28ba67db53bb7def8c
Jiwon Shin <jshin1394@gmail.com>
2025-04-03 13:15:52 -0700

    set keras backend to jax in classification_metrics_test (#57)

--- src/metrax/classification_metrics_test.py
+++ src/metrax/classification_metrics_test.py
@@ -14,6 +14,9 @@
 
 """Tests for metrax classification metrics."""
 
+import os
+os.environ['KERAS_BACKEND'] = 'jax'
+
 from absl.testing import absltest
 from absl.testing import parameterized
 import jax.numpy as jnp
